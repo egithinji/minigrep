88dd52794dafa5da859c57a22ce8d93f8007b555
Eric Githinji <ericgithinji@gmail.com>
2020-08-27 15:29:42 +0300

    Rename package name in use statements.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::process;
-use minigrep::Config;
+use minigrep_egithinji::Config;
 
 fn main() {
 
@@ -13,7 +13,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = minigrep_egithinji::run(config) {
 
         eprintln!("Application error: {}", e);
 
